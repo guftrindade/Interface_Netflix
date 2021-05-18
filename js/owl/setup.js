810e11f77fa61b94b1b187391ffefea253c56315
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2021-05-18 15:48:23 -0300

    Commit

---
+++ js/owl/setup.js
@@ -0,0 +1,60 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function redirecionar(){
+    window.open('https://www.youtube.com/watch?v=8QnMmpfKWvo&ab_channel=Netflix');
+}
+
+function redirecionar1(){
+    window.open('https://www.youtube.com/watch?v=5_4SW8HHfUs&ab_channel=NetflixBrasil');
+}
+
+function redirecionar2(){
+    window.open('https://www.youtube.com/watch?v=i3X-xQIjqws&ab_channel=Ingresso.com');
+}
+
+function redirecionar3(){
+    window.open('https://www.youtube.com/watch?v=d6bfyIKUYHE&ab_channel=Ingresso.com');
+}
+
+function redirecionar4(){
+    window.open('https://www.youtube.com/watch?v=3p1d_6_ocEE&ab_channel=MarvelBrasil');
+}
+
+function redirecionar5(){
+    window.open('https://www.youtube.com/watch?v=VKkFW5TTB6g&ab_channel=WarnerBros.PicturesBrasil');
+}
+
+function redirecionar6(){
+    window.open('https://www.youtube.com/watch?v=HhesaQXLuRY&ab_channel=TrailerBlend');
+}
+
+function redirecionar7(){
+    window.open('https://www.youtube.com/watch?v=MczMB8nU1sY&ab_channel=iflixMalaysia');
+}
+
+function redirecionar8(){
+    window.open('https://www.youtube.com/watch?v=gE4mgKBWZkE&ab_channel=D%27lives');
+}
+
+function redirecionar9(){
+    window.open('https://www.youtube.com/watch?v=yBHfuph4WWY&ab_channel=rosh1temp');
+}
+
+function redirecionar10(){
+    window.open('https://www.youtube.com/watch?v=t_ULBP6V9bg&ab_channel=Ingresso.com');
+}
